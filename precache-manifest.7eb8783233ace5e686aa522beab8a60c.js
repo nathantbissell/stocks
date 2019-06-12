9fc8dca5c6b8af323bb881a326d2b6d00c0f7f24
nathantbissell <nathantbissell@gmail.com>
2019-06-11 22:55:43 -0400

    Updates

---
+++ precache-manifest.7eb8783233ace5e686aa522beab8a60c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf38fc93852ff617158a",
+    "url": "/stocks/static/js/runtime~main.9551dd32.js"
+  },
+  {
+    "revision": "77c838025dbd0213b6b0",
+    "url": "/stocks/static/js/main.b0af56fe.chunk.js"
+  },
+  {
+    "revision": "a961bcce59b7a6221693",
+    "url": "/stocks/static/js/2.47bd0900.chunk.js"
+  },
+  {
+    "revision": "77c838025dbd0213b6b0",
+    "url": "/stocks/static/css/main.db19f871.chunk.css"
+  },
+  {
+    "revision": "a961bcce59b7a6221693",
+    "url": "/stocks/static/css/2.baf14a14.chunk.css"
+  },
+  {
+    "revision": "8d2ebd233a22c74a7a5cb94a3bc523fe",
+    "url": "/stocks/index.html"
+  }
+];
